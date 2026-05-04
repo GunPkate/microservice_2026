86cd84bd1a1bb1d81d6ed0bd34f978c9e9717055
LAPTOP-4V0UT6U6\Acer <gundash1@gmail.com>
2026-05-04 13:49:11 +0700

    init microservice order

---
+++ order/src/test/java/com/gunp/microservice/order/TestOrderApplication.java
@@ -0,0 +1,11 @@
+package com.gunp.microservice.order;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(OrderApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
