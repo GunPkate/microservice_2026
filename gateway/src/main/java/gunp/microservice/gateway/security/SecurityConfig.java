888789542e9b955a68ae63bcb26d0fd3f18a9d44
GunP <gundash1@gmail.com>
2026-07-03 10:29:31 +0700

    gateway add swagger and list of api service and allow urls

--- gateway/src/main/java/gunp/microservice/gateway/security/SecurityConfig.java
+++ gateway/src/main/java/gunp/microservice/gateway/security/SecurityConfig.java
@@ -15,9 +15,14 @@ public class SecurityConfig {
     @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri-cert}")
     private String OauthUrl;
 
+    private final String[] freeResourceUrls = {"/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**",
+            "/swagger-resources/**", "/api-docs/**", "/aggregate/**"};
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(freeResourceUrls)
+                        .permitAll()
                         .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
